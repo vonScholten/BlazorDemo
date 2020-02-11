b8c9415820faf2ebb807c0253ff34dae558c7d7c
vonScholten <22447372+vonScholten@users.noreply.github.com>
2020-02-11 14:37:21 +0100

    Added example: People

---
+++ BlazorDemo/Server/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<PersonModel> Get()
+        {
+            return new List<PersonModel>
+            {
+                new PersonModel{FirstName = "Frederik", LastName = "von Scholten", AccountBalance = 0.0M },
+                new PersonModel { FirstName = "Frederik I", LastName = "von Scholten", AccountBalance = 0.1M },
+                new PersonModel{FirstName = "Frederik II", LastName = "von Scholten", AccountBalance = 0.2M },
+                new PersonModel{FirstName = "Frederik III", LastName = "von Scholten", AccountBalance = 0.3M }
+            };   
+        }
+
+        [HttpPost]
+        public void Post(PersonModel person) { }
+    }
+}
---
+++ BlazorDemo/Shared/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDemo.Shared
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}

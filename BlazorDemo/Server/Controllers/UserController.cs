4051ed247a5441fa1ba38b44043e3bf38cbb8941
vonScholten <22447372+vonScholten@users.noreply.github.com>
2020-02-28 15:45:46 +0100

    Add fetch data table component

--- BlazorDemo/Server/Controllers/UserController.cs
+++ BlazorDemo/Server/Controllers/UserController.cs
@@ -18,8 +18,10 @@ namespace BlazorDemo.Server.Controllers
     {
       return new List<UserModel>
       {
-        new UserModel{Username = "Toothless", FirstName = "Frederik", LastName = "von Scholten", Img="https://d.wattpad.com/story_parts/497755278/images/15049796c1ac291360005534504.jpg" },
-        new UserModel{Username = "Light Fury", FirstName = "Pernille Marie", LastName = "Lindschov Hansen", Img="https://vignette.wikia.nocookie.net/loveinterest/images/4/4e/Light_Fury_Headshot.jpg/revision/latest?cb=20181011175457" },
+        new UserModel{Username = "Toothless", FirstName = "Frederik", LastName = "von Scholten", Img="https://d.wattpad.com/story_parts/497755278/images/15049796c1ac291360005534504.jpg", Email="example@example.com" },
+        new UserModel{Username = "Light Fury", FirstName = "Pernille Marie", LastName = "Lindschov Hansen", Img="https://vignette.wikia.nocookie.net/loveinterest/images/4/4e/Light_Fury_Headshot.jpg/revision/latest?cb=20181011175457", Email="example@example.com" },
+        new UserModel{Username = "Maverick", FirstName = "Mikkel", LastName = "Refsgaard Schmidt-Nielsen", Img="https://media1.s-nbcnews.com/j/newscms/2017_21/1216637/top-gun-movie-today-170524-inline-4_b7f9b1ce3dec3d29b5e181ee7481b46a.fit-760w.jpg", Email="example@example.com" },
+        new UserModel{Username = "Marocco", FirstName = "Iman", LastName = "Chelhi", Img="https://www.contrepoints.org/wp-content/uploads/2011/02/800px-Flag_of_Morocco_svg1.png", Email="example@example.com" },
       };
     }
 
